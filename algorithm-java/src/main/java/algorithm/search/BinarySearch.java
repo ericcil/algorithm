e5df6306664b76354c3cecc07ce139e58ac4b0c0
ericcil <emptyregister@163.com>
2017-07-06 15:17:24 +0800

    binary search

---
+++ algorithm-java/src/main/java/algorithm/search/BinarySearch.java
@@ -0,0 +1,28 @@
+package algorithm.search;
+
+/**
+ * 基于有序数组的二分查找
+ * 
+ */
+public class BinarySearch {
+
+	
+	/**
+	 * 示例
+	 * 基于有序数组实现的字典
+	 */
+	public class OrderedMap{
+		private String[] keys;//key数组
+		private String[] vals;//值数组
+		//keys数组和vals数组相同下标即为一对键值对
+		//由于keys是有序数组，则可以对keys进行二分查询
+		
+		public String get(String key){
+			return null;
+		}
+		
+		public void put(String key,String val){
+			
+		}
+	}
+}
