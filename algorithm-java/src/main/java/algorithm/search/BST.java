22b4813a6b4361d096c6f7a32883e371cc9c8f29
ericcil <emptyregister@163.com>
2017-07-07 16:37:43 +0800

    w

--- algorithm-java/src/main/java/algorithm/search/BST.java
+++ algorithm-java/src/main/java/algorithm/search/BST.java
@@ -33,6 +33,15 @@ public class BST {
 		else return x.val;
 	}
 
+	/**
+	 * put操作不会除了新增节点外，不会对树结构进行改变
+	 * key的插入顺序会直接影响树的结构，最好情况下树是左右平衡的
+	 * 最坏情况下树的高度会等于节点数，造成和基于无需数组的查找一样的开销
+	 * @param x
+	 * @param key
+	 * @param val
+	 * @return
+	 */
 	private Node put(Node x,String key,String val){
 		if(x == null) return new Node(key,val,1);//类似下沉操作
 		int cmp = key.compareTo(x.key);
@@ -45,6 +54,61 @@ public class BST {
 		}
 		x.n = size(x.left) + size(x.right) + 1;
 		return x;
+	}
+	
+	/**
+	 * 最小键
+	 * @return
+	 */
+	public String min(){
+		return min(root).key;
+	}
+	
+	/**
+	 * 选则第k个key
+	 * @param k
+	 * @return
+	 */
+	public String select(int k){
+		return select(root,k).key;
+	}
+	
+	private Node select(Node x,int k){
+		if(x == null) return null;
+		int t = size(x.left);
+		if(t > k){//如果k比左树的节点数小，则标识点k在左树里
+			return select(x.left,k);
+		}else if(t < k){
+			return select(x.right,k-t+1);//计算k相对于右树的位置
+		}else{
+			return x;
+		}
+	}
+	
+	/**
+	 * 获取给定key的位置
+	 * @param key
+	 * @return
+	 */
+	public int rank(String key){
+		return rank(key,root);
+	}
+	
+	private int rank(String key,Node x){
+		if(x == null) return 0;
+		int cmp = key.compareTo(x.key);
+		if(cmp < 0){
+			return rank(key,x.left);
+		}else if(cmp > 0){
+			return 1 + size(x.left) + size(x.right);
+		}else{
+			return size(x.left);
+		}
+	}
+	
+	public Node min(Node x){
+		if(x.left == null) return x;
+		return min(x.left);
 	}
 	
 	public class Node{
